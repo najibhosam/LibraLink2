5ae2d481d88ff4b8987a7767cb052452147be3cd
najibhosam <najibhosam@gmail.com>
2023-06-27 17:53:02 +0200

    viewmodelBook

--- app/src/main/java/com/example/libralink2/BookActivity.kt
+++ app/src/main/java/com/example/libralink2/BookActivity.kt
@@ -2,10 +2,21 @@ package com.example.libralink2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
+import com.example.libralink2.databinding.ActivityBookBinding
+import com.example.libralink2.databinding.ActivityMainBinding
 
 class BookActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityBookBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_book)
+
+        binding= ActivityBookBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var viewModel = ViewModelProvider(this).get(BookActivityViewModel::class.java)
+
+        viewModel.getInfo()
+
     }
 }
---
+++ app/src/main/java/com/example/libralink2/BookActivityViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.libralink2
+
+import androidx.lifecycle.ViewModel
+
+class BookActivityViewModel: ViewModel() {
+
+    var bookName = ""
+    var autherName = ""
+    var PublishingYear = ""
+    var Pages = 0
+    var category = ""
+
+    fun getInfo (){
+
+    }
+
+}
