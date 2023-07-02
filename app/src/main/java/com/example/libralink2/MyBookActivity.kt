006eaf7bb08182fef9e344b7344a6490b7b21c47
najibhosam <najibhosam@gmail.com>
2023-07-02 14:42:02 +0200

    end

--- app/src/main/java/com/example/libralink2/MyBookActivity.kt
+++ app/src/main/java/com/example/libralink2/MyBookActivity.kt
@@ -2,6 +2,10 @@ package com.example.libralink2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MyBookActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
