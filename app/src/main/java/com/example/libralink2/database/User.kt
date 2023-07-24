a9faf05542eb09f83f32da17a3c6a0b22c8eddd9
yanafedorchenko <65681994+yanafedorchenko@users.noreply.github.com>
2023-07-24 21:45:04 +0200

    Revert "Merge branch 'main' into old-project-state"
    This reverts commit d24c4aac39697775dd9982d7216085fe922466a3, reversing
    changes made to 4f51c18503e562456effddf9e653b9846b1afd4a.

--- app/src/main/java/com/example/libralink2/database/User.kt
+++ app/src/main/java/com/example/libralink2/database/User.kt
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "User_table")
+@Entity(tableName = "user")
 data class User(
     @PrimaryKey(autoGenerate = true)
     val userId: Int = 0,
--- app/src/main/java/com/example/libralink2/database/UserDao.kt
+++ app/src/main/java/com/example/libralink2/database/UserDao.kt
@@ -5,16 +5,8 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface UserDao {
-
-
-    @Query("SELECT * FROM User_table WHERE username LIKE :list_name")
-    suspend fun findUser (list_name : String):User
-
-   
-
-//=======
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertUser(User: User)
+    suspend fun insertUser(user: User)
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertList(list: BookList)
     @Insert(onConflict = OnConflictStrategy.IGNORE)
@@ -23,13 +15,12 @@ interface UserDao {
     suspend fun insertListBookCrossRef(crossRef: ListBookCrossRef)
     @Update
     suspend fun updateUser(user: User)
-//>>>>>>> 225ae67171e8376218acd0174941bdf734f6c8a1
     @Delete
     suspend fun deleteUser(user: User)
-    @Query("SELECT * from User_table WHERE userId = :userId")
+    @Query("SELECT * from user WHERE userId = :userId")
     fun getUser(userId: Int): Flow<User>
     @Transaction
-    @Query("SELECT * from User_table WHERE userId = :userId")
+    @Query("SELECT * from user WHERE userId = :userId")
     suspend fun getUserWithBookLists(userId: Int): List<UserWithBookLists>
     @Transaction
     @Query("SELECT * from book_list WHERE listId = :listId")
